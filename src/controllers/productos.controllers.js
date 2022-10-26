9534f944dd3b48596fd9173e55575e5ed45931bc
Nicolas Fara <nicolas.fara.romero@gmail.com>
2022-10-26 09:51:24 -0300

    Agregue la logica para obtener la lista de productos

--- src/controllers/productos.controllers.js
+++ src/controllers/productos.controllers.js
@@ -1,10 +1,38 @@
-export const listarProductos = (req, res) => {
-    res.send("desde el backend en la peticion get");
-      };
+import Producto from "../models/producto";
 
-export const crearProducto = (req, res) => {
-    console.log(req.body)
+export const listarProductos = async (req, res) => {
+  try {
+    //Buscar los productos
+    const productos = await Producto.find(); //buscar un obj con dato en particular find({categoria:'dulce'})
+
+    //Responder al frontend con el arreglo de productos
+    res.status(200).json(productos);
+  } catch (error) {
+    console.log(error);
+    //Enviar respuesta al frontend
+    res.status(404).json({
+      mensaje:'Error al buscar los productos'
+    });
+  }
+};
+
+export const crearProducto = async (req, res) => {
+  try {
+    console.log(req.body);
     //Validar los adtos del objeto
+    const productoNuevo = new Producto(req.body);
     //Guradar el objeto en la base de datos
-    res.send("desde el backend en la peticion post");
-      };
+    await productoNuevo.save();
+    res.status(201).json({
+      mensaje: "El producto se creo correctamente",
+    });
+
+    // Esta linea es para comprobar al principio que funcione todo bien
+    // res.send("desde el backend en la peticion post");
+  } catch (error) {
+    console.log(error);
+    res.status(404).json({
+      mensaje: "Error al intentar agregar un nuevo producto",
+    });
+  }
+};
