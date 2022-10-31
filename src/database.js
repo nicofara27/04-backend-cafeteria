d41b9be4f09b9afed593904d09ff338aeee984f8
Nicolas Fara <nicolas.fara.romero@gmail.com>
2022-10-31 11:20:29 -0300

    Agregue el link de produccion

--- src/database.js
+++ src/database.js
@@ -1,7 +1,8 @@
 import mongoose from "mongoose";
 
 //Crea la coleccion de la base de dato 127.0.0.1 es lo mismo a localhost
-const url = 'mongodb://127.0.0.1:27017/03-cafeteria';
+// const url = 'mongodb://127.0.0.1:27017/03-cafeteria'; Local
+const url = 'mongodb+srv://nicofara:nf38489829@cluster0.bf2rui7.mongodb.net/cafeteria'; // Produccion
 
 mongoose.connect(url);
 
